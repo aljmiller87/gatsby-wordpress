a97531a90870d522d5e17953eb54a438924e4ff0
Alex Miller <aljmiller87@gmail.com>
2020-02-11 10:01:33 -0500

    initial set up. Connect gatsby with wordpress source plugin to Local (by Flywheel) WP site

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,37 @@
 import React from "react"
-import { Link } from "gatsby"
+import { graphql, useStaticQuery } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+const query = graphql`
+  {
+    pages: allWordpressPage {
+      edges {
+        node {
+          title
+          slug
+          content
+        }
+      }
+    }
+  }
+`
+
+const IndexPage = () => {
+  const { pages } = useStaticQuery(query)
+
+  return (
+    <Layout>
+      {pages.edges.map(page => (
+        <div key={page.node.id}>
+          <h1>{page.node.title}</h1>
+          <div dangerouslySetInnerHTML={{ __html: page.node.content }} />
+        </div>
+      ))}
+    </Layout>
+  )
+}
 
 export default IndexPage
